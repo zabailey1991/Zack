58ffee7a9c72034148cdc8a86083d62eb4d45ce9
zabailey1991 <Zabailey1991@gmail.com>
2020-04-28 14:59:02 -0400

    Add files via upload

---
+++ pendo.js
@@ -0,0 +1,14 @@
+//Pendo Scripts
+//This is where you can install your Pendo scripts to setup your site with your Pendo subscription.  
+
+//Standard Pendo US Install
+
+
+//Standard Pendo EU Install
+
+
+//Segmnet Install of Pendo
+
+
+
+
